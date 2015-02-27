ee5dded1bd130a2cd99043abaade739b5f84100b
Nick Musaelian <nmusaelian@nmusaellian-mbr.f4tech.com>
2015-02-27 14:25:28 -0700

    commented

--- myexamples/unidir-data-flow/src/unidir-data-flow.js
+++ myexamples/unidir-data-flow/src/unidir-data-flow.js
@@ -1,37 +1,6 @@
-var FilteredList = React.createClass({
-    filterList:function(event){
-       var updatedList = this.state.initialItems;
-       updatedList = updatedList.filter(function(item){
-        return item.toLowerCase().search(
-            event.target.value.toLowerCase())!=-1;
-       });
-       this.setState({items: updatedList});
-    },
-    getInitialState:function(){
-        return {
-            initialItems:[
-                "Wendolene Ramsbottom",
-                "Lady Tottington",
-                "Piella Bakewell"
-            ],
-            items: []
-        }
-    },
-    componentWillMount: function(){
-        this.setState({items: this.state.initialItems})
-    },
-    render: function(){
-        return (
-            <div className="filter-list">
-                <input type="text" placeholder="Search" onChange={this.filterList}/>
-                <List items={this.state.items}/>
-            </div>
-        );
-    }
-});
-
 var List = React.createClass({
     render: function(){
+        console.log("rendering List");
         return (
             <ul>
             {
@@ -43,5 +12,46 @@ var List = React.createClass({
         )
     }
 });
+
+var FilteredList = React.createClass({
+    filterList:function(event){
+       var updatedList = this.state.initialItems;
+       updatedList = updatedList.filter(function(item){
+        return item.toLowerCase().search(
+            event.target.value.toLowerCase())!=-1;
+       });
+       this.setState({items: updatedList});
+    },
+    
+    //invoked once before the component is mounted
+    getInitialState:function(){
+        console.log('getInitialState');
+        return {
+            initialItems:[
+                "Wendolene Ramsbottom",
+                "Lady Tottington",
+                "Piella Bakewell"
+            ],
+            items: []
+        }
+    },
+    
+    //componentWillMount Ð Invoked once before rendering occurs.
+    componentWillMount: function(){
+        this.setState({items: this.state.initialItems}); //set "items" state to "initialItems"
+    },
+    render: function(){
+        console.log("rendering FilteredList");
+        //set "items" prop of List component to "items" state of FilteredList. Appears to be passed in from a parent via props.
+        return (
+            <div className="filter-list">
+                <input type="text" placeholder="Search" onChange={this.filterList}/>
+                <List items={this.state.items}/> 
+            </div>
+        );
+    }
+});
+
+
 
 React.render(<FilteredList/>,document.getElementById('my-div'));
